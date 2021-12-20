957005276dd9056c1edc1f597ded2f4b151ec26e
sunikko <sunikko77@gmail.com>
2021-12-20 00:55:29 +0000

    Edit admin panel list : test

--- users/admin.py
+++ users/admin.py
@@ -6,4 +6,6 @@ from . import models
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
     """ Custom User Admin """
-    list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+    list_display = ("username", "email", "gender",
+                    "language", "currency", "superhost")
+    list_filter = ("superhost", "language", "currency")
