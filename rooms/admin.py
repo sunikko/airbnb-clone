30dfe89512d77a0a184135824930ad0b6742940b
Soy <sunikko77@gmail.com>
2022-01-10 23:34:46 +0000

    create room models:

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ItemAdmin Definition"""
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Photo Admin Definition"""
+
     pass
