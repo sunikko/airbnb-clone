eb3d7aa82be2ded227640ab2df85138fde9b5b1a
Soy <soy@Soys-Mac-mini.local>
2022-01-10 23:32:30 +0000

    create base abstract model:

--- core/models.py
+++ core/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class TimeStampModel(models.Model):
 
-    """ Time Stamped Model """
-    created = models.DateTimeField()
-    update = models.DateTimeField()
+    """Time Stamped Model"""
 
-    # it's Abstract not go into db
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    # it's Abstract not go into db /Meta means extra infomation
     class Meta:
         abstract = True
